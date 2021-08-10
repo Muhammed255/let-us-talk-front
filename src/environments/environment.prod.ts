936cae42511fdccea92c1fbec67fab5263847dce
Mohamed Ahmed <programmer2011515616@gmail.com>
2021-08-10 13:14:25 +0200

    adding some changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://we-call-text.herokuapp.com/"
+  apiUrl: "https://we-call-text-backend.herokuapp.com/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://we-call-text.herokuapp.com/"
+  apiUrl: "https://we-call-text-backend.herokuapp.com/"
 };
 
 /*
